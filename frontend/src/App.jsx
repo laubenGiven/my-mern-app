9e617f2bc5780190cf8fa8c9cd5e56f23eb520f1
laubenGiven <mpairwelauben375@gmail.com>
2024-01-25 12:24:27 +0300

    3rd commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
+import FileUpload from "./components/FileUpload";
 
 function App() {
   return (
     <>
       <span> Subscribe Alien Technologies code </span>
       <Header />
+      <h1> Image Upload</h1>
+      <FileUpload />
     </>
   );
 }
