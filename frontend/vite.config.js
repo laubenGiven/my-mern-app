16aedd47ffbbc6664ca712454eb1e9dde1cdc425
laubenGiven <mpairwelauben375@gmail.com>
2024-01-26 19:29:59 +0300

    from local repo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/upload': 'http://localhost:3000'
-  //   }
-  // }
+  server: {
+    proxy: {
+      // Proxying all requests starting with /api to the backend server
+      '/api': {
+        target: 'http://localhost:5000', // Your backend server
+        changeOrigin: true,
+        secure: false,
+        ws: true, // proxy websockets if needed
+        rewrite: (path) => path.replace(/^\/api/, '') // remove /api
+      },
+    },
+  },
 })
