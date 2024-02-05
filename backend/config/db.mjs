74dfd0f142c95f14d80b5bda8e9b415bed59448c
landwind <landwind@localhost.localdomain>
2024-02-05 19:44:39 +0300

    latest commit

--- backend/config/db.mjs
+++ backend/config/db.mjs
@@ -7,10 +7,7 @@ const { DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb://${DB_USERNAME}:${DB_PASSWORD}@localhost:27017/${DB_NAME}?authSource=admin`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(`mongodb://${DB_USERNAME}:${DB_PASSWORD}@localhost:27017/${DB_NAME}?authSource=admin` );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Header from "./components/Header";
-import FileUpload from "./components/FileUpload";
+
 import RegistrationForm from "./RegistrationForm";
 
 function App() {
